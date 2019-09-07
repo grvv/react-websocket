26511ae3d60eea7bb0e6f9a80e8f001604b5359d
Gaurav Sharma <gaurav.gauravsharmaa@gmail.com>
2019-09-07 05:39:21 +0530

    minior fix

--- src/components/HomePage/NoMessages.jsx
+++ src/components/HomePage/NoMessages.jsx
@@ -4,7 +4,7 @@ const emptyMessageBox = require("../../images/empty-box.png");
 const NoMessages = () => {
   return (
     <div className="flex-grow-1 p-2 text-center">
-      <img src={emptyMessageBox} className="mt-5" alt="No Messages" />
+      <img src={emptyMessageBox} className="img-fluid mt-4" alt="No Messages" />
       <h1 className="text-center font-weight-light mt-3">No Messages...</h1>
     </div>
   );
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -7,6 +7,13 @@ import validate from "./validation";
 import { connect } from "react-redux";
 import { login } from "../../actions";
 
+styleObject = {
+  display: "flex",
+  height: "100vh",
+  flexDirection: "column",
+  justifyContent: "center"
+};
+
 class Login extends Component {
   onSubmit = formValues => {
     this.props.login(formValues);
@@ -14,29 +21,31 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="card m-5">
-        <div className="card-body">
-          <h1 className="text-center font-weight-light">Login</h1>
-          <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-            <Field
-              name="email"
-              component={Input}
-              label="Email"
-              placeholder="Enter Email"
-              type="text"
-            />
-            <Field
-              name="password"
-              component={Input}
-              label="Password"
-              placeholder="Enter Password"
-              type="password"
-            />
+      <div className="container" style={this.styleObject}>
+        <div className="card m-5">
+          <div className="card-body">
+            <h1 className="text-center font-weight-light">Login</h1>
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+              <Field
+                name="email"
+                component={Input}
+                label="Email"
+                placeholder="Enter Email"
+                type="text"
+              />
+              <Field
+                name="password"
+                component={Input}
+                label="Password"
+                placeholder="Enter Password"
+                type="password"
+              />
 
-            <div className="text-center">
-              <button className="btn btn-primary">Login</button>
-            </div>
-          </form>
+              <div className="text-center">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
