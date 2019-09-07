f3c6af69b489f1f0543a1ca6e1c04a11a1a7705e
Gaurav Sharma <gaurav.gauravsharmaa@gmail.com>
2019-09-07 05:50:51 +0530

    add build

---
+++ build/precache-manifest.b9490062e43efc4260c8f4b8267f0a7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f39a8c5e6e3121cbe3d4151d470ba8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e2d306a2f202e55800",
+    "url": "/static/css/2.56d65a16.chunk.css"
+  },
+  {
+    "revision": "01e2d306a2f202e55800",
+    "url": "/static/js/2.c3c92497.chunk.js"
+  },
+  {
+    "revision": "8540cf844de0f55b1947",
+    "url": "/static/js/main.3976ecbb.chunk.js"
+  },
+  {
+    "revision": "8e7cdc25b8f79c129665",
+    "url": "/static/js/runtime~main.a57ec374.js"
+  }
+]);
